5428b5e43e95d6f8a73b43c5cc1ef855e32282fc
sonerxdev <sonerkaraevli@gmail.com>
2022-04-08 01:51:50 +0300

    Added write and read func in core data. Added NotificationCenter for state management. Works very well.

--- ShoppingList/AddItemViewController.swift
+++ ShoppingList/AddItemViewController.swift
@@ -6,16 +6,73 @@
 //
 
 import UIKit
+import CoreData
 
-class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
 
-    @IBOutlet weak var itemNameTxt: UITextField!
-    @IBOutlet weak var itemPriceTxt: UITextField!
-    @IBOutlet weak var itemSizeTxt: UITextField!
+ 
+    @IBOutlet weak var imageNameTxt: UITextField!
+    
+    @IBOutlet weak var imagePriceTxt: UITextField!
+    @IBOutlet weak var imageSizeTxt: UITextField!
     @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedItemName: String = ""
+    var selectedItemUUID : UUID?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if selectedItemName != "" {
+            
+            if let uuidString = selectedItemUUID?.uuidString{
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Shopping")
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                
+                do {
+                    let results = try context.fetch(fetchRequest)
+                    
+                    if results.count > 0 {
+                        for result in results as! [NSManagedObject]{
+                            
+                            if let name = result.value(forKey: "name") as? String {
+                                imageNameTxt.text = name
+                            }
+                            if let price = result.value(forKey: "price") as? Int {
+                                imagePriceTxt.text = String(price)
+                            }
+                            
+                            if let size = result.value(forKey: "size") as? String {
+                                imageSizeTxt.text = size
+                            }
+                            
+                            if let imageData = result.value(forKey: "image") as? Data {
+                                let image = UIImage(data: imageData)
+                                imageView.image = image
+                                
+                            }
+                        }
+                    }
+                }
+                catch{
+                    debugPrint("problem")
+                }
+                
+            }
+            
+        }
+        else {
+            imageNameTxt.text = ""
+            imagePriceTxt.text = ""
+            imageSizeTxt.text = ""
+        }
         
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
@@ -44,5 +101,35 @@ class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     @IBAction func saveBtn(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let shopping = NSEntityDescription.insertNewObject(forEntityName: "Shopping", into: context)
+        
+        shopping.setValue(imageNameTxt.text!, forKey: "name")
+        shopping.setValue(imageSizeTxt.text!, forKey: "size")
+        
+        if let price = Int(imagePriceTxt.text!){
+            shopping.setValue(price, forKey: "price")
+        }
+        
+        shopping.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        
+        shopping.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            debugPrint("saved")
+        }
+        catch{
+            debugPrint("problem") 
+        }
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "dataAdded"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+        
+        
     }
 }
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -6,19 +6,100 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    var selectedName = ""
+    var selectedUUID : UUID?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addBtn))
+        
+        getData()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "dataAdded"), object: nil)
+    }
+    
+   @objc func getData(){
+       
+       nameArray.removeAll(keepingCapacity: false)
+       idArray.removeAll(keepingCapacity: false)
+       
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Shopping")
+        fetchRequest.returnsObjectsAsFaults = false
+       
+       
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0 {
+                for result in results as! [NSManagedObject]{
+                    if let name = result.value(forKey: "name") as? String {
+                        nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        idArray.append(id)
+                    }
+                    
+                }
+                tableView.reloadData()
+            }
+        }
+        
+        catch{
+            debugPrint("problem")
+        }
     }
     
     @objc func addBtn(){
+        selectedName = ""
         performSegue(withIdentifier: "toAddItemVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    //prepare for segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toAddItemVC" {
+            let destination = segue.destination as! AddItemViewController
+            destination.selectedItemName = selectedName
+            destination.selectedItemUUID = selectedUUID
+        }
+    }
+    
+    //did select row func tıklanılan itemden sonra ne yapılacağını belirtir
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedName = nameArray[indexPath.row]
+        selectedUUID = idArray[indexPath.row]
+        performSegue(withIdentifier: "toAddItemVC", sender: nil)
+
     }
 
 }
